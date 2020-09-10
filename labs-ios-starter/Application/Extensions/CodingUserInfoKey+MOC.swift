50855c44717631b39fcb17522090c7e0f5e226c9
Shawn James <shawn.james@me.com>
2020-09-10 13:38:19 -0400

    Extend CodingUserInfoKey with MOC support

---
+++ labs-ios-starter/Application/Extensions/CodingUserInfoKey+MOC.swift
@@ -0,0 +1,6 @@
+// Copyright © 2020 Lambda, Inc. All rights reserved.
+// CodingUserInfoKey+MOC.swift
+
+extension CodingUserInfoKey {
+    static let managedObjectContext = CodingUserInfoKey(rawValue: "managedObjectContext")!
+}
