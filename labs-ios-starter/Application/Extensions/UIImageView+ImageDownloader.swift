d9fa61c713f0e53e646ec17d632903f0994f6570
Shawn James <57146228+Shawn-James@users.noreply.github.com>
2020-09-11 10:56:42 -0400

    Update image downloader to use networking classes

--- labs-ios-starter/Application/Extensions/UIImageView+ImageDownloader.swift
+++ labs-ios-starter/Application/Extensions/UIImageView+ImageDownloader.swift
@@ -6,9 +6,16 @@ import UIKit
 extension UIImageView {
     func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFill) {
         contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        
+        let networkService = NetworkService()
+        guard let request = networkService.createRequest(url: url, method: .get) else {
+            print("Failed to create request, check URL")
+            return
+        }
+        
+        networkService.dataLoader.loadData(using: request) { data, response, error in
             guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let httpURLResponse = response, httpURLResponse.statusCode == 200,
                 let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
                 let data = data, error == nil,
                 let image = UIImage(data: data)
@@ -16,7 +23,7 @@ extension UIImageView {
             DispatchQueue.main.async() { [weak self] in
                 self?.image = image
             }
-        }.resume()
+        }
     }
     // Helper method
     func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFill) {
