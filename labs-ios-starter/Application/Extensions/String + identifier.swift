f69cd64418433d0736b6974cb840299ffd27f091
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-09-12 08:13:14 -0700

    Separate MemberList from User Profile (create storyboard)

---
+++ labs-ios-starter/Application/Extensions/String + identifier.swift
@@ -0,0 +1,25 @@
+//
+//  String + identifier.swift
+//  labs-ios-starter
+//
+//  Created by Kenny on 9/12/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+enum StoryboardIdentifier: String {
+    case profileDetailViewController = "ProfileDetailViewController"
+}
+///convenience method to retrieve storyboardIdentifiers
+func getStoryboardIdentifier(identifier: StoryboardIdentifier) -> String {
+    identifier.rawValue
+}
+
+enum SegueIdentifier: String {
+    case sample
+}
+///convenience method to get Segue Identifiers
+func getSegueIdentifier(identifier: SegueIdentifier) -> String {
+    identifier.rawValue
+}
