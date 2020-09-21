d6f6ab943ca30a1da6b06f29a3f690696602cba0
Shawn James <shawn.james@me.com>
2020-09-21 16:24:29 -0400

    Clean up & format

--- labs-ios-starter/Surveys/Model Controllers/TopicController.swift
+++ labs-ios-starter/Surveys/Model Controllers/TopicController.swift
@@ -70,61 +70,56 @@ class TopicController {
     }
     
     // MARK: - Read -
-    
+
     /// fetch all topics, or topics for the currently logged in user
     /// - Parameters:
     ///   - all: fetch topics for all users (`true`) or just the currently logged in user (`false`)
     ///   - completion: Completes with `[Topic]`. Topics are also stored in the controller...
     func fetchTopic(all: Bool = false, completion: @escaping CompleteWithTopics) {
-        
-        
         guard let request = createRequest(pathFromBaseURL: "topic") else {
             print("🛑! User isn't logged in!")
             completion(.failure(.unauthorized))
             return
         }
-        
+
         networkService.loadData(using: request) { result in
             switch result {
-                case .success(let data):
-                    guard let topics = self.networkService.decode(to: [Topic].self,
-                                                                  data: data,
-                                                                  moc: CoreDataManager.shared.mainContext) else {
-                                                                    print("Error decoding topics")
-                                                                    completion(.failure(.badDecode))
-                                                                    return
+            case .success(let data):
+                guard let topics = self.networkService.decode(to: [Topic].self,
+                                                              data: data,
+                                                              moc: CoreDataManager.shared.mainContext) else {
+                    print("Error decoding topics")
+                    completion(.failure(.badDecode))
+                    return
+                }
+                var userTopics: [Topic] = []
+                if !all {
+                    guard let user = ProfileController.shared.authenticatedUserProfile else {
+                        print("🛑! User isn't logged in!")
+                        completion(.failure(.unauthorized))
+                        return
                     }
-                    var userTopics: [Topic] = []
-                    if !all {
-                        guard let user = ProfileController.shared.authenticatedUserProfile else {
-                            print("🛑! User isn't logged in!")
-                            completion(.failure(.unauthorized))
-                            return
-                        }
-                        // This is going to be quite the expensive check if there are a lot of topics with a lot of members
-                        for topic in topics {
-                            
-                            if topic.leaderId == user.id { // member is leader
+                    // This is going to be quite the expensive check if there are a lot of topics with a lot of members
+                    for topic in topics {
+                        if topic.leaderId == user.id { // member is leader
+                            userTopics.append(topic)
+                        } else if let members = topic.members { // member is user
+                            if members.contains(user) {
                                 userTopics.append(topic)
-                            } else if let members = topic.members { // member is user
-                                if members.contains(user) {
-                                    userTopics.append(topic)
-                                }
                             }
-                            
                         }
                     }
-                    completion(.success(userTopics))
-                
-                case .failure(let error):
-                    // bubble error to caller
-                    completion(.failure(error))
+                }
+                completion(.success(userTopics))
+
+            case .failure(let error):
+                // bubble error to caller
+                completion(.failure(error))
             }
         }
     }
-    
-    
-    ///Get all contexts
+
+    /// Get all contexts
     func getAllContexts(complete: @escaping CompleteWithNeworkError) {
         guard let request = createRequest(pathFromBaseURL: "context") else {
             print("couldn't get context, invalid request")
@@ -132,83 +127,82 @@ class TopicController {
         }
         networkService.loadData(using: request) { result in
             switch result {
-                case .success(let data):
-                    guard let contexts = self.networkService.decode(to: [ContextObject].self,
-                                                                    data: data,
-                                                                    moc: CoreDataManager.shared.mainContext) else {
-                                                                        print("error decoding contexts from valid data")
-                                                                        complete(.failure(.notFound))
-                                                                        return
-                    }
-                    self.CONTEXTS = contexts
-                    complete(.success(Void()))
-                //bubble error to caller
-                case .failure(let error):
-                    complete(.failure(error))
+            case .success(let data):
+                guard let contexts = self.networkService.decode(to: [ContextObject].self,
+                                                                data: data,
+                                                                moc: CoreDataManager.shared.mainContext) else {
+                    print("error decoding contexts from valid data")
+                    complete(.failure(.notFound))
+                    return
+                }
+                self.CONTEXTS = contexts
+                complete(.success(Void()))
+            // bubble error to caller
+            case let .failure(error):
+                complete(.failure(error))
             }
         }
     }
-    
+
     func getQuestions(completion: @escaping CompleteWithNeworkError) {
         // create request to /question
-        guard let request = self.createRequest(pathFromBaseURL: "question") else {
+        guard let request = createRequest(pathFromBaseURL: "question") else {
             completion(.failure(.badRequest))
             return
         }
-        
+
         // get questions from endpoint
-        self.networkService.loadData(using: request) { result in
-            //self is nil here???
+        networkService.loadData(using: request) { result in
+            // self is nil here???
             switch result {
-                // decode questions
-                case .success(let data):
-                    guard let questions = self.networkService.decode(to: [Question].self,
-                                                                     data: data,
-                                                                     moc: CoreDataManager.shared.mainContext) else {
-                                                                        completion(.failure(.badDecode))
-                                                                        return
-                    }
-                    self.QUESTIONS = questions
-                    completion(.success(Void()))
-                
-                // bubble error to caller
-                case .failure(let error):
-                    completion(.failure(error))
+            // decode questions
+            case .success(let data):
+                guard let questions = self.networkService.decode(to: [Question].self,
+                                                                 data: data,
+                                                                 moc: CoreDataManager.shared.mainContext) else {
+                    completion(.failure(.badDecode))
+                    return
+                }
+                self.QUESTIONS = questions
+                completion(.success(Void()))
+
+            // bubble error to caller
+            case .failure(let error):
+                completion(.failure(error))
             }
         }
     }
-    
+
     func getAllQuestionsAndContexts(completion: @escaping CompleteWithNeworkError) {
         getAllContexts { contextResult in
             switch contextResult {
-                case .success:
-                    self.getQuestions() { result in
-                        completion(result)
+            case .success:
+                self.getQuestions { result in
+                    completion(result)
                 }
-                case.failure:
-                    completion(contextResult)
+            case .failure:
+                completion(contextResult)
             }
         }
     }
-    
+
     // MARK: - Helper Methods -
-    
+
     private func createRequest(auth: Bool = true,
                                pathFromBaseURL: String,
                                method: NetworkService.HttpMethod = .get) -> URLRequest? {
         let targetURL = baseURL.appendingPathComponent(pathFromBaseURL)
-        
+
         guard var request = networkService.createRequest(url: targetURL, method: method, headerType: .contentType, headerValue: .json) else {
             print("unable to create request for \(targetURL)")
             return nil
         }
-        
+
         if auth {
-            //add bearer to request
+            // add bearer to request
             request.addAuthIfAvailable()
         }
-        
+
         return request
     }
-    
 }
