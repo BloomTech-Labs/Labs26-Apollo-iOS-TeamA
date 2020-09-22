095cca54ca5b99e90fdf90baa079720033e6c192
Shawn James <shawn.james@me.com>
2020-09-22 13:57:45 -0400

    Update partial view-model for dynamically managing headers

--- labs-ios-starter/Surveys/View Models/TopicCVSections.swift
+++ labs-ios-starter/Surveys/View Models/TopicCVSections.swift
@@ -1,15 +1,22 @@
 // Copyright © 2020 Shawn James. All rights reserved.
 // TopicCVSections.swift
 
+import UIKit
+
 /// Sets the sections headers for the collectionView on the TopicViewController. Order sensitive. Description is label text.
 enum TopicCVSections: Int, CaseIterable, CustomStringConvertible {
-    case myTopics
-    case joined
+    case leader
+    case member
     
     var description: String {
         switch self {
-            case .myTopics: return "My Topics"
-            case .joined: return "Advanced Settings"
+            case .leader: return "Leader"
+            case .member: return "Member"
         }
     }
+}
+
+/// This links with the storyboard to configure the cells appearance and make it reusable view.
+class TopicSectionHeader: UICollectionReusableView {
+    @IBOutlet var sectionHeaderLabel: UILabel!
 }
