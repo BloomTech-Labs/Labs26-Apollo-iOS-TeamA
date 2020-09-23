b2e6b31ab58486f6b794877d1dbb25f443521112
Shawn James <shawn.james@me.com>
2020-09-23 11:33:36 -0400

    Update NotificationsViewController to populate with NotificationsMessages

---
+++ labs-ios-starter/Surveys/View/NotificationsCollectionViewCell.swift
@@ -0,0 +1,8 @@
+// Copyright © 2020 Shawn James. All rights reserved.
+// NotificationsCollectionViewCell.swift
+
+import UIKit
+
+class NotificationsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var messageLabel: UILabel!
+}
