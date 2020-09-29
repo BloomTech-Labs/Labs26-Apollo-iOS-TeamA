15754e11f6da84f61ca66bc45a61e01eaea9ff03
Shawn James <shawn.james@me.com>
2020-09-28 23:43:36 -0400

    Clean up

---
+++ labs-ios-starter/Surveys/View/TopicSectionHeader.swift
@@ -0,0 +1,9 @@
+// Copyright © 2020 Shawn James. All rights reserved.
+// TopicSectionHeader.swift
+
+import UIKit
+
+/// This links with the storyboard to configure the cells appearance and make it reusable view.
+class TopicSectionHeader: UICollectionReusableView {
+    @IBOutlet var sectionHeaderLabel: UILabel!
+}
