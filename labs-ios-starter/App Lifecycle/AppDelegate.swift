a5adcd0de2e15fe266ad8f775bc74329abe64c99
Shawn James <shawn.james@me.com>
2020-10-02 21:41:20 -0400

    Update global font attributes

--- labs-ios-starter/App Lifecycle/AppDelegate.swift
+++ labs-ios-starter/App Lifecycle/AppDelegate.swift
@@ -13,6 +13,19 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UserDefaults.standard.set(false, forKey: .notificationsVCdidLoad)
+
+        let labelAppearance = UILabel.appearance()
+        labelAppearance.font = UIFont(name: "Apple Symbols", size: 26)
+        
+        let titleAppearance = UINavigationBar.appearance()
+        titleAppearance.titleTextAttributes = [.font : UIFont(name: "AppleSDGothicNeo-Bold", size: 25)!]
+        titleAppearance.largeTitleTextAttributes = [.font : UIFont(name: "AppleSDGothicNeo-Bold", size: 41)!]
+        
+        let tabBarAppearance = UITabBarItem.appearance()
+        tabBarAppearance.setTitleTextAttributes([.font : UIFont(name: "Apple Symbols", size: 16)!], for: .normal)
+        
+        UIButton.appearance().titleLabel?.font = UIFont(name: "Apple Symbols", size: 26) // not working
+
         return true
     }
 
