fe8d49136f53fa3195c86202469e6fd3a90bf016
Shawn James <shawn.james@me.com>
2020-10-07 14:31:41 -0400

    Re-add file with new id in the pbxproj

---
+++ labs-ios-starter/Application/Operators/GuardDebugOperator.swift
@@ -0,0 +1,23 @@
+// Copyright © 2020 Lambda, Inc. All rights reserved.
+// Created by Shawn James
+// GuardDebugOperator.swift
+
+// MARK: - Guard Statement Debugging Operator
+infix operator ><
+
+/// Used in guard statements to print an error message before failing.
+/// ```
+/// guard
+///     let middleName = middleName >< "Missing middle name"
+/// else {
+///     print("Exited early from functionName()")
+/// }
+/// ```
+/// - Parameters:
+///   - lhs: The optional that is being unwrapped in the guard statement
+///   - rhs: The message that you would like to print if the guard statement fails
+/// - Returns: The unwrapped object or nil
+func >< <T>(lhs: T?, rhs: String) -> T? {
+    if lhs == nil { print(rhs) }
+    return lhs
+}
