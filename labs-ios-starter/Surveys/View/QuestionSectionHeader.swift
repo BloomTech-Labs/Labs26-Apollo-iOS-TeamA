1f8d1c01b62144f263d790479ab01cc9515090f5
Shawn James <shawn.james@me.com>
2020-10-20 16:58:49 -0400

    Add QuestionsSectionHeader

---
+++ labs-ios-starter/Surveys/View/QuestionSectionHeader.swift
@@ -0,0 +1,12 @@
+// Copyright © 2020 Lambda, Inc. All rights reserved.
+// Created by Shawn James
+// QuestionSectionHeader.swift
+
+import UIKit
+
+/// This links with the storyboard to configure the cells appearance and make it reusable view.
+class QuestionSectionHeader: UICollectionReusableView {
+    @IBOutlet var questionNameLabel: UILabel!
+
+    @IBAction func respondButtonPressed(_ sender: UIButton) {}
+}
